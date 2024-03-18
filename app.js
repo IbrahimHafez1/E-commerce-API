c38c664856301ac3fa20c669e4b6c6fa83e7e896
Ibrahim Hafez <ibrahimhafez1@yahoo.com>
2024-03-18 02:59:40 +0200

    product model and routes

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
 require('express-async-errors')
 
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
+const productsRouter = require('./routes/productRoutes')
 
 dotenv.config()
 
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static('./public'))
+app.use(fileUpload({
+    useTempFiles: true
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -28,6 +34,7 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/products', productsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
---
+++ controllers/productController.js
@@ -0,0 +1,69 @@
+const Product = require('../models/Product')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+const path = require('path')
+
+const createProduct = async (req, res) => {
+    req.body.user = req.user.userId
+    const product = await Product.create(req.body)
+    return res.status(StatusCodes.CREATED).json({ product })
+}
+
+const getAllProducts = async (req, res) => {
+    const products = await Product.find({})
+    return res.status(StatusCodes.OK).json({ products })
+}
+
+const getSingleProduct = async (req, res) => {
+    const product = await Product.findOne({ _id: req.params.id })
+    if (!product) {
+        throw new CustomError.NotFoundError('No product found')
+    }
+    return res.status(StatusCodes.OK).json({ product })
+}
+
+const updateProduct = async (req, res) => {
+    const product = await Product.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        returnDocument: 'after',
+        runValidators: true
+    })
+    if (!product) {
+        throw new CustomError.NotFoundError(`No product with id : ${productId}`)
+    }
+    return res.status(StatusCodes.OK).json({ product })
+}
+
+const deleteProduct = async (req, res) => {
+    const product = await Product.findOne({ _id: req.params.id })
+    if (!product) {
+        throw new CustomError.NotFoundError(`No product with id : ${productId}`)
+    }
+    await product.remove()
+    return res.status(StatusCodes.OK).json({ msg: 'Success! Product removed' })
+}
+
+const uploadImage = async (req, res) => {
+    if (!req.files) {
+        throw new CustomError.BadRequestError('No file uploaded')
+    }
+    const productImage = req.files.image
+    if (!productImage.mimetype.startsWith('image')) {
+        throw new CustomError.BadRequestError('Please upload image')
+    }
+    const maxSize = 1024 * 1024
+    if (productImage.size > maxSize) {
+        throw new CustomError.BadRequestError('Please upload image smaller than 1mb')
+    }
+    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`)
+    await productImage.mv(imagePath)
+    return res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` })
+}
+
+module.exports = {
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadImage
+}
