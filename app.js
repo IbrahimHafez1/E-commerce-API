f7adb5c6e3e159af4909c51e6e9ee6900ec889d6
Ibrahim Hafez <ibrahimhafez1@yahoo.com>
2024-03-20 00:00:46 +0200

    add orders and security

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
+const cors = require('cors')
 require('express-async-errors')
 
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productsRouter = require('./routes/productRoutes')
 const reviewsRouter = require('./routes/reviewRoutes')
+const orderRouter = require('./routes/orderRoutes')
 
 dotenv.config()
 
@@ -24,6 +30,15 @@ app.use(express.static('./public'))
 app.use(fileUpload({
     useTempFiles: true
 }))
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60
+}))
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+app.use(cors())
+app.set('trustProxy', 1)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -37,6 +52,7 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productsRouter)
 app.use('/api/v1/reviews', reviewsRouter)
+app.use('/api/v1/orders', orderRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
--- models/Review.js
+++ models/Review.js
@@ -32,4 +32,33 @@ const reviewSchema = new mongoose.Schema({
 
 reviewSchema.index({ product: 1, user: 1 }, { unique: true })
 
+reviewSchema.statics.getAverageRating = async function (productId) {
+    const result = await this.aggregate([
+        {
+            $match: { product: productId }
+        },
+        {
+            $group: {
+                _id: null,
+                averageRating: { $avg: '$rating' },
+                numOfReviews: { $sum: 1 }
+            }
+        }
+    ])
+    try {
+        await this.model('Product').findByIdAndUpdate(productId, {
+            averageRating: Math.ceil(result[0].averageRating || 0)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+reviewSchema.post('save', async function () {
+    await this.constructor.getAverageRating(this.product)
+})
+reviewSchema.post('remove', async function () {
+    await this.constructor.getAverageRating(this.product)
+})
+
 module.exports = mongoose.model('Review', reviewSchema)
